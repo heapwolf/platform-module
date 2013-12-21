30a7365248248be5df4a5dcd16476695e181df3f
Paolo Fragomeni <paolo@async.ly>
2013-12-21 10:35:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-
 ;((typeof module !== 'undefined' && function(m) { module.exports = m[1]; }) ||
-  (typeof define === 'function' && function(m) { define(name(m[0]), function() { return m[1] }); }) ||
+  (typeof define === 'function' && function(m) { define(m[0], function() { return m[1] }); }) ||
   (function(m) { window[m[0]] = m[1]; })
 )(function(m) { var name = /^function\s*(.*?)\s*\(/; return [name.exec(m.toString())[1], m]; }(
 
